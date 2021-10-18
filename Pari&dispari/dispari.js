d11c87976b2e1cfdbfae405dc56ee8bc9f36e26f
FrancescoAndreotti <francescoandreotti00@gmail.com>
2021-10-18 17:48:35 +0200

    Pari e dispari

--- Pari&dispari/dispari.js
+++ Pari&dispari/dispari.js
@@ -8,7 +8,28 @@ const numUtente = document.querySelector("[name='number']");
 
 
 btnSubmit.addEventListener("click", function () {
+
     const pOD = pariODispari.value;
-    const number = numUtente.value;
-    console.log(pOD, number);
+    let num = numUtente.value;
+    console.log(pOD, num);
+
+    let numAi = Math.ceil(Math.random() * 5);
+    console.log(numAi);
+    num = parseInt(num);
+    numAi = parseInt(numAi);
+    const somma = num + numAi;
+    console.log(somma);
+
+    let oddOrEven = "";
+    if (somma % 2 === 0) {
+        oddOrEven = "pari";
+    } else {
+        oddOrEven = "dispari";
+    }
+
+    if (oddOrEven === pOD) {
+        alert("Complimenti hai vinto!");
+    } else {
+        alert("Mi dispiace hai perso!");
+    }
 });
